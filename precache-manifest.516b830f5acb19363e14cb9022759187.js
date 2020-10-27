4a84bda99e5a7f202b703dd3ef2a2125ef11974a
Josipa Dadić <josipa.dadic@gmail.com>
2020-10-27 20:28:58 +0100

    Updates

---
+++ precache-manifest.516b830f5acb19363e14cb9022759187.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60a438e76c47ed99055f93014ae3247",
+    "url": "/jo-dadic/jochatapp/index.html"
+  },
+  {
+    "revision": "447c5b12b5bee4acf711",
+    "url": "/jo-dadic/jochatapp/static/css/main.7d747a77.chunk.css"
+  },
+  {
+    "revision": "669c40c0cc6df58aafcb",
+    "url": "/jo-dadic/jochatapp/static/js/2.d3c8007b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jo-dadic/jochatapp/static/js/2.d3c8007b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "447c5b12b5bee4acf711",
+    "url": "/jo-dadic/jochatapp/static/js/main.13b26c71.chunk.js"
+  },
+  {
+    "revision": "ab4e7d7935121be56bd8",
+    "url": "/jo-dadic/jochatapp/static/js/runtime-main.9356c6ea.js"
+  }
+]);
